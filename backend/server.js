5cf24b9fa661d5b12c4989762516f8005a7c86a1
Meetsuthar32778 <sutharmeet513@gmail.com>
2024-07-17 12:52:01 +0530

    creating backend server with name 'app' using express.

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,23 @@ const express=require("express");
 const mysql=require("mysql2");
 const cors=require("cors");
 
-{// Getting Access or database from MySQL
-    const db=mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'4878$Meet',
-        database:'ediglobe'
-    });
-    console.log("MySQL has been activated.");
+// Getting Access or database from MySQL
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'4878$Meet',
+    database:'ediglobe'
+});
 
-    db.connect(console.log("Database has been connected successfully."))
-}
+db.connect(console.log("Database has been connected successfully."))
 
 
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(2024,()=>{
+    console.log("Server is running on PORT 2024.");
+})
